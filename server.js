fdc7df8e2b670e455c8df8aa843094de3e4c240c
Mayank Jain <mayank13@gmail.com>
2016-10-21 19:54:04 +0530

    Fix for ex-1

--- server.js
+++ server.js
@@ -2,4 +2,28 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+var jsonData = {
+	count: 12,
+	message: 'hey'
+};
+var fs = require('fs');
+var express = require('express');
+var app = express();
+// On Hitting the Root, it should return the html file
+
+app.get('/', function(req, res) {
+	fs.readFile('./index.html', (err, data) => {
+		if (err) throw err;
+		console.log(data);
+		// Send the HTMl response
+		res.send(data);
+	});
+});
+
+app.get('/data', function(req, res) {
+	res.json(jsonData);
+});
+
+
+// start server on port 3000
+app.listen(3000);
